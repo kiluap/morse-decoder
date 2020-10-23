29adfe8f6a4a9b0755243f8a96852c74448f8d01
Pavel Skrypko <V_8_P@mail.ru>
2020-10-23 19:21:04 +0300

    fix: fixed

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrChar = [];
+    let strMorse = '';
+    let str = '';
+    arrChar = expr.match(/.{1,10}/g);
+    for (let i = 0; i < arrChar.length; i++){
+        let arrMorse = arrChar[i].match(/.{1,2}/g)
+        if (arrChar[i] === '**********'){
+           str += ' '; 
+           continue; 
+        } 
+        for (let j = 0; j < arrMorse.length; j++){
+            if (arrMorse[j] === '10') strMorse +='.';
+            if (arrMorse[j] ==='11') strMorse +='-';
+        }
+        str += MORSE_TABLE[strMorse];
+        strMorse = '';
+    }
+    return str;
+
 }
 
 module.exports = {
